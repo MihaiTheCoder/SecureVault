190f6db15c88b43b60f27864a72c4df2f3cc2040
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2018-08-14 16:24:03 +0300

    Add import image from gallery

--- app/src/main/java/com/mihaiapps/securevault/MainFragment.kt
+++ app/src/main/java/com/mihaiapps/securevault/MainFragment.kt
@@ -1,6 +1,10 @@
 package com.mihaiapps.securevault
 
+import android.Manifest
+import android.app.Activity
+import android.content.ClipData
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
@@ -10,11 +14,15 @@ import android.widget.ImageButton
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.github.florent37.runtimepermission.RuntimePermission.askPermission
 import com.mihaiapps.securevault.bl.LocalFileReader
 import com.mihaiapps.securevault.bl.enc.EncryptUtils
 import com.mihaiapps.securevault.bl.enc.EncryptedFileManager
+import kotlinx.android.synthetic.main.fragment_main.*
 import org.koin.android.ext.android.inject
 import java.io.File
+
+
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -44,6 +52,22 @@ class MainFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        import_images_btn.setOnClickListener { _ ->
+            askPermission(this)
+                    .request(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    .onAccepted { _ ->
+                        val intent = Intent()
+                        intent.type = "image/*"
+                        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                        intent.action = Intent.ACTION_GET_CONTENT
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_SELECT_IMAGE)
+                    }.ask()
+
+        }
+    }
+
     private lateinit var mCurrentPhotoPath: String
 
     private fun createImageFile(): File {
@@ -70,7 +94,6 @@ class MainFragment : Fragment() {
             val photoURI = FileProvider.getUriForFile(context!!,FILE_PROVIDER_AUTHORITH, photoFile)
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
             startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
-
         }
     }
 
@@ -80,11 +103,38 @@ class MainFragment : Fragment() {
                     mCurrentPhotoPath + EncryptUtils.FILE_EXTENSION,
                     true)
         }
+        if(requestCode == REQUEST_SELECT_IMAGE) {
+            if(resultCode == Activity.RESULT_OK && data != null) {
+                val clipData = data.clipData
+                if(clipData != null) {
+                    val count = clipData.itemCount
+                    for (i in 0 until count) {
+                        val imageUri = clipData.getItemAt(i).uri
+                        encryptLocalImage(imageUri)
+                    }
+                } else if(data.data != null) {
+                    encryptLocalImage(data.data!!)
+
+                }
+            }
+
+        }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun encryptLocalImage(imageUri: Uri) {
+
+        val encryptedFilePath = localFileReader.getAppsImageDirectory(imageUri) +
+                EncryptUtils.FILE_EXTENSION
+
+        val path = localFileReader.getRealPathFromURI(imageUri)
+
+        encryptedFileManager.encryptFile(path!!, encryptedFilePath, false)
     }
 
     companion object {
         const val REQUEST_TAKE_PHOTO = 1
+        const val REQUEST_SELECT_IMAGE = 2
         const val FILE_PROVIDER_AUTHORITH = "com.mihaiapps.securevault.fileprovider"
 
     }
--- app/src/main/java/com/mihaiapps/securevault/bl/LocalFileReader.kt
+++ app/src/main/java/com/mihaiapps/securevault/bl/LocalFileReader.kt
@@ -1,10 +1,16 @@
 package com.mihaiapps.securevault.bl
 
 import android.content.Context
+import android.net.Uri
 import android.os.Environment
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStream
+import android.provider.OpenableColumns
+import android.provider.MediaStore
+import android.content.ContentUris
+import android.provider.DocumentsContract
+import android.content.ContentResolver
 
 class LocalFileReader(private val context: Context) {
     companion object {
@@ -31,7 +37,58 @@ class LocalFileReader(private val context: Context) {
     fun deleteFiles(files: List<String>) {
         for (file in files) {
             File(file).delete()
-
         }
     }
+
+    fun getAppsImageDirectory(unencryptedGalleryUri: Uri): String? {
+        val filName = getFileName(unencryptedGalleryUri)
+        return File(getPicturesDirectory(), filName).absolutePath
+    }
+
+    fun getFileName(uri: Uri): String {
+        var result: String? = null
+        if (uri.scheme == "content") {
+            val cursor = context.contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor!!.close()
+            }
+        }
+        if (result == null) {
+            result = uri.path
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1)
+            }
+        }
+        return result
+    }
+
+    fun getRealPathFromURI(uri: Uri): String {
+        var filePath = ""
+        val wholeID = DocumentsContract.getDocumentId(uri)
+
+        // Split at colon, use second item in the array
+        val id = wholeID.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
+
+        val column = arrayOf(MediaStore.Images.Media.DATA)
+
+        // where id is equal to
+        val sel = MediaStore.Images.Media._ID + "=?"
+
+        val cursor = context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                column, sel, arrayOf(id), null)
+
+        val columnIndex = cursor!!.getColumnIndex(column[0])
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex)
+        }
+        cursor.close()
+        return filePath
+    }
+
 }
